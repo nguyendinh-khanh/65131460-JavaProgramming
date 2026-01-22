fb95eba98f5f058d09cd3a67a7b50bb88c636729
Nguyễn Đình Khánh <khanhdinh2680@gmail.com>
2026-01-22 16:36:11 +0700

    Bài 1: BTTH 1 HelloWorld
    Thực hiện bài thực hành 1 trên trang tiilaz.com. Thay đổi nội dung in ra màn hình, in thêm thông tin cá nhân của bạn

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-
-        System.out.println("Hello World");
+        System.out.println("Họ và tên: Nguyễn Đình Khánh");
+        System.out.println("MSSV: 65131460");
+        System.out.println("Lớp: 65-CNTT2");
     }
 }
