5cc178b48efb0d2acf6cae596db0b943de6433c0
Nguyễn Đình Khánh <khanhdinh2680@gmail.com>
2026-01-22 16:51:04 +0700

    Bài 1: BTTH 3 Tính toán đơn giản

---
+++ TinhToan/src/TinhToan.java
@@ -0,0 +1,10 @@
+public class TinhToan {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 3;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
