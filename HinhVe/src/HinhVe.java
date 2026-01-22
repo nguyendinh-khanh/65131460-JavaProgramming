757362765ae6834e42f83b0a053dc9fb6b7d618f
Nguyễn Đình Khánh <khanhdinh2680@gmail.com>
2026-01-22 16:40:52 +0700

    Bài 1: BTTH 2 HinhVe
    Thực hiện bài thực hành 2 trên trang tiilaz.com. In hình ảnh đơn giản

---
+++ HinhVe/src/HinhVe.java
@@ -0,0 +1,9 @@
+public class HinhVe
+{
+    public static void main(String[] args) {
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  *** ");
+        System.out.println("   * ");
+    }
+}
