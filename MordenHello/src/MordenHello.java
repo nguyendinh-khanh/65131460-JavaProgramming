db6ca202a3f1fc9374de48ea6a6061d88f57d23e
Nguyễn Đình Khánh <khanhdinh2680@gmail.com>
2026-01-24 22:07:05 +0700

    Bài 1: BTTH 4A

---
+++ MordenHello/src/MordenHello.java
@@ -0,0 +1,14 @@
+public class MordenHello {
+    public static void main(String[] args){
+        String thongTin = """
+                ==================================
+                HELLO, MODERN JAVA!
+                ==================================
+                Ho va ten: Nguyen Dinh Khanh
+                MSSV     : 65131460
+                Lop      : CNTT-@
+                ==================================
+                """;
+        System.out.println(thongTin);
+    }
+}
