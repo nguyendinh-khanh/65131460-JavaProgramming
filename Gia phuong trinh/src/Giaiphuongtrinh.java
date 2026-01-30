7cb273a8148b08813b483b23b2c85d21d12ec0e7
Nguyễn Đình Khánh <khanhdinh2680@gmail.com>
2026-01-30 15:30:42 +0700

    Bài 2 BTTH 3  Giải phương trình bậc 1

---
+++ Gia phuong trinh/src/Giaiphuongtrinh.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Giaiphuongtrinh {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Giai phuong trinh: ax + b = 0");
+        // Nhập a và b
+        System.out.print("Nhap he so a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhap he so b: ");
+        double b = scanner.nextDouble();
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phuong trinh co VO SO NGHIEM.");
+            } else {
+                System.out.println("Phuong trinh VO NGHIEM.");
+            }
+        } else {
+            // Công thức: x = -b / a
+            double x = -b / a;
+            System.out.printf("x = %.2f", x);
+        }
+    }
+}
