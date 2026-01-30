babd790b4e8675f87f121ca7adda8814ffaec01b
Nguyễn Đình Khánh <khanhdinh2680@gmail.com>
2026-01-30 15:08:53 +0700

    Bài 2 BTTH: 2 Quy đổi tiền tệ

---
+++ Doi Tien/src/DoiTien.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class DoiTien {
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+
+        //khai báo biến không thay đổi
+        final double USD = 23500;
+        final double EUR = 27000;
+       
+        System.out.print("Nhap so tien can doi: ");
+        double soTien = scanner.nextDouble();
+        
+        // Tính theo USD
+        double UsdSangVnd = soTien * USD;
+        System.out.printf("Tu %.2f USD sang VND la %,.0f VND\n", soTien, UsdSangVnd);
+
+        // Tính theo EUR
+        double EurSangVnd = soTien * EUR;
+        System.out.printf("Tu %.2f EUR sang VND la %,.0f VND\n", soTien, EurSangVnd);
+    }
+}
